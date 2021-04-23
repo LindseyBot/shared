10f7b1299d0dfda9d409e03e287d8c34ff2b413c
Fabricio Winter <git@notfab.net>
2021-04-22 21:51:02 -0300

    Fix deserialization issues

--- src/main/java/net/notfab/lindsey/shared/entities/commands/response/ErrorResponse.java
+++ src/main/java/net/notfab/lindsey/shared/entities/commands/response/ErrorResponse.java
@@ -2,9 +2,11 @@ package net.notfab.lindsey.shared.entities.commands.response;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import net.notfab.lindsey.shared.entities.commands.CommandResponse;
 
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
 public class ErrorResponse implements CommandResponse {
 
