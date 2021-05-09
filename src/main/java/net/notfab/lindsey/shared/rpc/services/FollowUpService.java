7691bf51f2a41beb3f02ef822bc1566c4c8a0563
Fabricio Winter <git@notfab.net>
2021-05-08 23:39:27 -0300

    Better docs

--- src/main/java/net/notfab/lindsey/shared/rpc/services/FollowUpService.java
+++ src/main/java/net/notfab/lindsey/shared/rpc/services/FollowUpService.java
@@ -8,14 +8,72 @@ public interface FollowUpService {
         return "FollowUpService";
     }
 
+    /**
+     * Sends a message on a specific text channel.
+     * <p>
+     * This operation is ASYNC, validation errors will throw IllegalArgumentException,
+     * execution errors will result in a expired interaction.
+     *
+     * @param guildId   Id of the guild.
+     * @param channelId Id of the TextChannel.
+     * @param message   Message contents.
+     * @return Interaction id, used for follow-ups.
+     */
     long sendMessage(long guildId, long channelId, Message message);
 
+    /**
+     * Updates a specific message, creating a new interaction.
+     * <p>
+     * This operation is ASYNC, validation errors will throw IllegalArgumentException,
+     * execution errors will result in a expired interaction.
+     *
+     * @param guildId   Id of the guild.
+     * @param channelId Id of the TextChannel.
+     * @param messageId Id of the message.
+     * @param message   Message contents.
+     * @return Interaction id, used for follow-ups.
+     */
     long editMessage(long guildId, long channelId, long messageId, Message message);
 
+    /**
+     * Updates a message by referencing the interaction.
+     * <p>
+     * This operation is ASYNC, validation errors will throw IllegalArgumentException,
+     * execution errors will result in a expired interaction.
+     * <p>
+     * This operation has a max lifetime of 5 minutes after the initial interaction was created.
+     *
+     * @param interaction Interaction id.
+     * @param message     Message contents.
+     * @return The same interaction id as passed previously.
+     */
     long editMessage(long interaction, Message message);
 
+    /**
+     * Deletes a message by referencing the interaction.
+     * <p>
+     * This operation is ASYNC, validation errors will throw IllegalArgumentException,
+     * execution errors will result in a expired interaction.
+     * <p>
+     * This operation has a max lifetime of 5 minutes after the initial interaction was created.
+     *
+     * @param interaction Interaction id.
+     */
     void deleteMessage(long interaction);
 
-    long reply(long interaction, Message message);
+    /**
+     * Replies to a message by referencing the interaction.
+     * <p>
+     * This operation is ASYNC, validation errors will throw IllegalArgumentException,
+     * execution errors will result in a expired interaction.
+     * <p>
+     * This operation has a max lifetime of 5 minutes after the initial interaction was created.
+     *
+     * @param interaction Interaction id.
+     * @param message     Message contents.
+     * @param mention     If the original author should be mentioned.
+     * @return New interaction id, corresponding to the reply.
+     */
+    long reply(long interaction, Message message, boolean mention);
 
 }
